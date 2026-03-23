444a9f0e1883ef1af8fd08fa88f117b8301e7c5e
sasha <oleksandr.ivaniuk@nure.ua>
2026-03-23 19:32:32 +0200

    Додано новий C# файл program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+Console.Write("Введіть число 1"); int num = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (num == 1)
+    {
+        Console.WriteLine("Hello GitHub!");
+        break;
+    } else
+    {
+        Console.WriteLine("Введіть число 1");
+    }
+}
